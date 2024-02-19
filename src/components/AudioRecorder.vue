4de074acdb14657cb5b4db33548df461560a82de
azykdev <azyk9114@gmail.com>
2024-02-19 12:05:00 +0500

    #1 - Complete Audio recorder v1

---
+++ src/components/AudioRecorder.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <audio-recorder upload-url="YOUR_API_URL" :attempts="3" :time="2" :headers="headers" :before-recording="callback"
+      :pause-recording="callback" :after-recording="callback" :select-record="callback" :before-upload="callback"
+      :successful-upload="callback" :failed-upload="callback" />
+  </main>
+</template>
+
+<script>
+export default {
+  methods: {
+    callback(data) {
+      console.debug(data)
+    }
+  }
+}
+</script>
+
+<style scoped>
+main {
+  text-align: center;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import AudioRecorder from "vue-audio-recorder";
 
-Vue.config.productionTip = false
+Vue.use(AudioRecorder);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
