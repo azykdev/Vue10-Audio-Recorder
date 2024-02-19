59e0b526941b5d1882c01e97789a10d643fe434d
azykdev <azyk9114@gmail.com>
2024-02-19 16:25:24 +0500

    #2 - change bg img

--- src/components/AudioRecorder.vue
+++ src/components/AudioRecorder.vue
@@ -1,8 +1,8 @@
 <template>
   <main>
-    <audio-recorder upload-url="YOUR_API_URL" :attempts="3" :time="2" :headers="headers" :before-recording="callback"
-      :pause-recording="callback" :after-recording="callback" :select-record="callback" :before-upload="callback"
-      :successful-upload="callback" :failed-upload="callback" />
+    <audio-recorder style="background-color: #ffffffde; color: black;" upload-url="YOUR_API_URL" :attempts="3" :time="2"
+      :headers="headers" :before-recording="callback" :pause-recording="callback" :after-recording="callback"
+      :select-record="callback" :before-upload="callback" :successful-upload="callback" :failed-upload="callback" />
   </main>
 </template>
 
@@ -23,5 +23,9 @@ main {
   display: flex;
   align-items: center;
   justify-content: center;
+  background: linear-gradient(to right, #0f2027c1, #203a43b1, #2c5364), url('../assets/bg.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 }
 </style>
